1cea35b394266be09b313765eebf57d8439730c8
Maribel <115499421+Maribelhdz04@users.noreply.github.com>
2022-11-21 13:31:07 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (849202, 0, -1207968.482);
+
+--UPDATE sandbox SET int64_value = 21548 WHERE id = 25;
+
+--DELETE FROM sandbox WHERE id = 35;
